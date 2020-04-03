8981426ff4aae4799117b640b8d4307b0f097c74
SATHISH VUPPALA <>
2020-04-03 00:58:44 -0400

    first commit to parent

---
+++ src/main/java/com/example2/AzArtifactoryTest2Application.java
@@ -0,0 +1,13 @@
+package com.example2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzArtifactoryTest2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzArtifactoryTest2Application.class, args);
+	}
+
+}
